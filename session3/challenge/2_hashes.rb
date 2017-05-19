62da8ca2bdba225421104d7760868e30b4958c3a
Miho Tse <mihotse@gmail.com>
2017-05-19 10:47:49 -0400

    Ch3:2 Hashes

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,15 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase number
+  answer = {}
+  1.upto number do |given_num|
+    # start from 1 and count up to given_num
+    next if given_num.even?
+    complete = Array.new(given_num) {|index| index + 1}
+    # made index = same num as given_num
+    even_nums = complete.select { |index| index.even? }
+    answer[given_num] = even_nums
+  end
+  return answer
+end

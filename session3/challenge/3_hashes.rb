f97e5e043ca4c1ef79b7528089f01a7040ee5a9e
Miho Tse <mihotse@gmail.com>
2017-05-19 11:02:29 -0400

    Ch3:3 Hashes

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,10 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count string
+  words = string.downcase.split(" ")
+  tally = Hash.new(0)
+  words.each { |word| tally[word] += 1}
+  return tally
+end

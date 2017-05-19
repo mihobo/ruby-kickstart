159e404164bc39cf6b57081e9fd6ba4e08270001
Miho Tse <mihotse@gmail.com>
2017-05-19 11:56:20 -0400

    Ch3:4 Hashes

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,11 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos string
+  words = string.split(" ")
+  position = {}
+  words.each_with_index { |word, index| position[word] ||= index}
+  # if left side has a set value ignore right side, otherwise sets new value on right side
+  return position
+end

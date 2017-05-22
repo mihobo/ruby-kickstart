627c733e9a6ca892ce704c1fe03ea4f0fbb4c073
Miho Tse <mihotse@gmail.com>
2017-05-22 18:35:24 -0400

    Ch3:5 Blocks

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,60 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+
+def spiral_access(arrays, &block)
+  # checks if arrays has no sub-arrays
+  if arrays.empty?
+    return
+  end
+
+# top row
+  first_set = arrays[0]
+  first_set.each do |x|
+    block.call x
+  end
+  #removes the first array (top row)
+  arrays.shift
+
+  if arrays.empty?
+    return
+  end
+
+# right column
+  second_set = arrays.transpose.last
+  second_set.each do |x|
+    block.call x
+  end
+  # removes the end values of each array (right column)
+  arrays.map(&:pop)
+
+  if arrays.empty?
+    return
+  end
+
+# bottom row
+  third_set = arrays[-1].reverse
+  third_set.each do |x|
+    block.call x
+  end
+  # removes the last array (bottom row)
+  arrays.pop
+
+  if arrays.empty?
+    return
+  end
+
+# left column
+  fourth_set = arrays.transpose.first.reverse
+  fourth_set.each do |x|
+    block.call x
+  end
+  # removes the first values of the arrays (left column)
+  arrays.map(&:shift)
+
+  # recursive step
+  spiral_access(arrays, &block)
+
+
+end
